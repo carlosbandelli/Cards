b70ac586f25b82cee23199158c98ed6d4b90a1bf
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2022-08-04 00:28:43 -0300

    Troca de axios para fetch

--- src/utils/load-post.jsx
+++ src/utils/load-post.jsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
+
 
 export const loadPosts = async () => {
-    const postsResponse = axios.get('http://jsonplaceholder.typicode.com/posts');
-    const photosResponse = axios.get('http://jsonplaceholder.typicode.com/photos');
+  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+  const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
   
     const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
   
-    const postsJson = await posts.data;
-    const photosJson = await photos.data;
+    const postsJson = await posts.json();
+  const photosJson = await photos.json();
   
     const postsAndPhotos = postsJson.map((post, index) => {
       return { ...post, cover: photosJson[index].url };
